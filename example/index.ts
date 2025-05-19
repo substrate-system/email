385fc52cd01cef28d1bedc267d74371efa3be713
nichoth <nichoth@nichoth.com>
2025-05-18 17:18:52 -0700

    wip

--- example/index.ts
+++ example/index.ts
@@ -1,6 +1,35 @@
+import { SubstrateButton } from '@substrate-system/button'
+import { SubstrateEmail } from '../src/index.js'
+import './style.css'
 import '../src/index.css'
-import '../src/index.js'
+import '@substrate-system/css-normalize'
+import '@substrate-system/button/css'
+import Debug from '@substrate-system/debug'
+const debug = Debug()
 
-document.body.innerHTML += `
-    <substrate-email></substrate-email>
+SubstrateEmail.define()
+SubstrateButton.define()
+const qs = document.querySelector.bind(document)
+
+// @ts-expect-error dev
+window.qs = document.querySelector.bind(document)
+
+document.getElementById('main')!.innerHTML += `
+    <form>
+        <substrate-email label="email" name="alice" label="email"></substrate-email>
+        <div id="controls">
+            <substrate-button disabled>Submit</substrate-button>
+        </div>
+    </form>
 `
+
+const input = qs('substrate-email')
+debug('the email input', input)
+
+input?.addEventListener('valid', ev => {
+    debug('We are valid!', ev)
+})
+
+input?.addEventListener('invalid', ev => {
+    debug('no longer valid....', ev)
+})
--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,56 @@
+import { Input } from '@substrate-system/input'
 import { createDebug } from '@substrate-system/debug'
 const debug = createDebug()
 
 // for docuement.querySelector
 declare global {
     interface HTMLElementTagNameMap {
-        'substrate-email': Example
+        'substrate-email': SubstrateEmail
     }
 }
 
-export class Example extends HTMLElement {
+export class SubstrateEmail extends Input {
     // Define the attributes to observe
     // need this for `attributeChangedCallback`
-    static observedAttributes = ['example']
+    static observedAttributes = ['name', 'id']
+    static tag = 'substrate-email'
 
-    example:string|null
+    hasFocused:boolean
+    hasBlurred:boolean
+    _shouldErr:boolean = false
 
     constructor () {
         super()
-        const example = this.getAttribute('example')
-        this.example = example
-
-        this.innerHTML = `<div>
-            <p>example</p>
-            <ul>
-                ${Array.from(this.children).filter(Boolean).map(node => {
-                    return `<li>${node.outerHTML}</li>`
-                }).join('')}
-            </ul>
-        </div>`
+        this.hasFocused = false
+        this.hasBlurred = false
     }
 
-    /**
-     * Handle 'example' attribute changes
-     * @see {@link https://gomakethings.com/how-to-detect-when-attributes-change-on-a-web-component/#organizing-your-code Go Make Things article}
-     *
-     * @param  {string} oldValue The old attribute value
-     * @param  {string} newValue The new attribute value
-     */
-    handleChange_example (oldValue:string, newValue:string) {
-        debug('handling example change', oldValue, newValue)
-
-        if (newValue === null) {
-            // [example] was removed
-        } else {
-            // set [example] attribute
-        }
+    get label () {
+        return this.getAttribute('label')
     }
 
+    get hasValue ():boolean {
+        return !!(this.input?.value)
+    }
+
+    // /**
+    //  * @see {@link https://gomakethings.com/how-to-detect-when-attributes-change-on-a-web-component/#organizing-your-code Go Make Things article}
+    //  */
+    // handleChange_name (_oldValue, newValue) {
+    //     if (!this.input) return
+    //     this.input.name = newValue
+    // }
+
     /**
-     * Runs when the value of an attribute is changed
+     * Runs when the value of an observed attribute is changed
      *
      * @param  {string} name     The attribute name
      * @param  {string} oldValue The old attribute value
-     * @param  {string} newValue The new attribute value
+     * @param  {string} newValue The new attributhis, te value
      */
     attributeChangedCallback (name:string, oldValue:string, newValue:string) {
-        debug('an attribute changed', name)
         const handler = this[`handleChange_${name}`];
-        (handler && handler(oldValue, newValue))
+        (handler && handler.call(this, oldValue, newValue))
         this.render()
     }
 
@@ -66,33 +59,150 @@ export class Example extends HTMLElement {
     }
 
     connectedCallback () {
-        debug('connected')
-
-        const observer = new MutationObserver(function (mutations) {
-            mutations.forEach((mutation) => {
-                if (mutation.addedNodes.length) {
-                    debug('Node added: ', mutation.addedNodes)
-                }
-            })
+        this.render()
+        const input = this.input
+        input?.addEventListener('blur', () => {
+            this.hasBlurred = true
+            this._showError()
         })
 
-        observer.observe(this, { childList: true })
+        input?.addEventListener('focus', () => {
+            this.hasBlurred = false
+            this.hasFocused = true
+            this._showError()
+        })
 
-        this.render()
+        input?.addEventListener('input', () => {
+            this._showError()
+        })
     }
 
-    render () {
-        this.innerHTML = `<div>
-            <p>example</p>
-            <ul>
-                ${Array.from(this.children).filter(Boolean).map(node => {
-                    return `<li>${node.outerHTML}</li>`
-                }).join('')}
-            </ul>
-        </div>`
+    get isValid ():boolean {
+        if (this.hasAttribute('required')) {
+            return !!(this.input?.value && isValid(this.input.value))
+        } else {
+            // is not required
+            if (!this.input?.value) {
+                return true
+            }
+            return isValid(this.input!.value)
+        }
+    }
+
+    _shouldShowError ():boolean {
+        this._shouldErr = (
+            !this.isValid &&
+            this.hasFocused &&
+            this.hasBlurred
+        )
+
+        return this._shouldErr
+    }
+
+    _showError () {
+        const email = this.querySelector('input')
+        const label = this.querySelector('label')
+        const shouldErr = this._shouldShowError()
+
+        if (!shouldErr) {
+            // don't show an error
+            email?.classList.remove('error')
+            label?.classList.remove('error')
+            if (this.classList.contains('error')) {
+                // if there is an error message, then remove it
+                label?.removeChild(label.lastChild!)
+                this.classList.remove('error')
+            }
+
+            return
+        }
+
+        // else, shouldErr is true; we are invalid, should show message
+        let msg
+        if (this.hasAttribute('required')) {
+            if (!this.hasValue) {
+                msg = this.getAttribute('requiredmsg') || 'Email is required.'
+                email?.classList.add('error')
+                email?.setAttribute('aria-invalid', 'true')
+            } else {
+                // is required, and has value, but the value is invalid
+                email?.classList.add('error')
+                label?.classList.add('error')
+                email?.setAttribute('aria-invalid', 'true')
+                msg = (this.getAttribute('errormsg') ||
+                    'Please use a valid email address.')
+            }
+        } else {
+            // not required, but should show error
+            email?.classList.add('error')
+            label?.classList.add('error')
+            email?.setAttribute('aria-invalid', 'true')
+            msg = (this.getAttribute('errormsg') ||
+                'Please use a valid email address.')
+        }
+
+        const el = label?.querySelector('.error')
+        if (el) {
+            el.innerHTML = msg
+        } else {
+            label?.insertAdjacentHTML(
+                'beforeend',
+                `<span class="error">${msg}</span>`
+            )
+        }
+
+        this.classList.add('error')
+    }
+
+    render ():void {
+        this.innerHTML = `
+            <label>
+                <span>${this.label}</span>
+            </label>
+            ${super.render()}
+        `
     }
 }
 
-if ('customElements' in window) {
-    customElements.define('substrate-email', Example)
+// // check email validity
+// email?.addEventListener('input', (ev) => {
+//     const input = ev.currentTarget as HTMLInputElement
+//     const email = input?.value.trim()
+//     if (!email) {
+//         startBatch()
+//         emailHasValue(false)
+//         emailOk(false)
+//         endBatch()
+//     }
+//     const parts = email.split('.')
+
+//     const isValid = (
+//         email.split('@').length === 2 &&
+//         parts.length > 1 &&
+//         parts.reduce((ok, part) => {
+//             // no consecutive dots, no start or end with dot
+//             return !!(ok && part)
+//         }, true)
+//     )
+
+//     if (isValid !== emailOk()) {
+//         startBatch()
+//         emailHasValue(!!email)
+//         emailOk(isValid)
+//         endBatch()
+//     }
+
+//     emailHasValue(!!email)
+// })
+
+export function isValid (email:string):boolean {
+    const parts = email.split('.')
+
+    return (email.split('@').length === 2 &&
+        parts.length > 1 &&
+        parts.reduce((ok, part) => {
+            // no consecutive dots, no start or end with dot
+            return !!(ok && part)
+        }, true)
+    )
 }
