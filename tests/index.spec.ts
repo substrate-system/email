3bb68ccd46e141019aa7392774e637ef788bf720
nichoth <nichoth@nichoth.com>
2025-05-19 13:30:15 -0700

    tests run

---
+++ tests/index.spec.ts
@@ -0,0 +1,33 @@
+import { expect, test } from 'vitest'
+import { SubstrateEmail } from '../src/index.js'
+
+test('get started', () => {
+    expect(SubstrateEmail).toBeTruthy()
+})
+
+// import { test } from '@substrate-system/tapzero'
+// import { waitFor, type } from '@bicycle-codes/dom'
+// import '../src/index.js'
+
+// let el
+// test('Create the element', async t => {
+//     document.body.innerHTML += `
+//         <substrate-email class="test">
+//         </substrate-email>
+//     `
+
+//     el = await waitFor('substrate-email')
+
+//     t.ok(el, 'should find an element')
+// })
+
+// test('events', async t => {
+//     t.plan(1)
+
+//     el.addEventListener('invalid', ev => {
+//         t.ok(true, 'should get an event')
+//         console.log('**inv ev**', ev)
+//     })
+
+//     type(el, 'hello')
+// })
